f6abd3f56bee127f818e13371821eccf01f1dd41
Kraken <mario.ballestserosp@gmail.com>
2022-10-08 09:38:34 -0500

    :c

---
+++ src/main/java/com/mycompany/leccion1/Calendar.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.leccion1;
+
+/**
+ *
+ * @author mario
+ */
+public class Calendar { 
+    public static final int DAYS_PER_WEEK = 7;
+    
+}
